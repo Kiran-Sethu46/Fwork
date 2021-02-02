555e564e94dc170291d57d523159ac00038d49a6
Kiran-Sethu46 <kiran226242@gmail.com>
2021-02-02 18:49:34 +0530

    starting

--- src/app/views/forms/FormsRoutes.js
+++ src/app/views/forms/FormsRoutes.js
@@ -3,12 +3,8 @@ import React from "react";
 const formsRoutes = [
   {
     path: "/forms/basic",
-    component: React.lazy(() => import("./BasicForm"))
+    component: React.lazy(() => import("./BasicForm")),
   },
-  {
-    path: "/forms/editor",
-    component: React.lazy(() => import("./EditorForm"))
-  }
 ];
 
 export default formsRoutes;
